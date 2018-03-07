562524cbc25dd044d1e4d1a0eca66062f4de0998
Ricardo García Fernández <ricardogarfe@gmail.com>
2018-03-07 12:38:17 +0100

    Move scrips to vars

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,10 +1,13 @@
 @Library('ricardogarfe/jenkins-shared-pipeline') _
 standardPipeline {
+
     projectName = "Project1"
     serverDomain = "Project1 Server Domain"
+
 }
+
 node {
     stage ('Notify') {
-        notifySlack this, '${text}', '${slackURL}', '${channel}', '${attachments}' 	
+        notifyToSlack '${text}', '${slackURL}', '${channel}', '${attachments}' 	
     }
 }
---
+++ vars/notifyToSlack.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+import groovy.json.JsonOutput
+
+def call(def text, def slackURL, def channel, def attachments) {
+
+    def jenkinsIcon = 'https://wiki.jenkins-ci.org/download/attachments/2916393/logo.png'
+
+    def payload = JsonOutput.toJson([text: text,
+        channel: channel,
+        username: "Jenkins",
+        icon_url: jenkinsIcon,
+        attachments: attachments
+    ])
+
+    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
+}
